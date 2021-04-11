c7a0552ce65e85c95440aed9665e2b726532985f
Alex Li <alex_li@asus.com>
2021-04-11 18:02:47 +0800

    Remove hard coded date when fetching daily trends

--- lib/ui/google_trend/google_trend_view_model.dart
+++ lib/ui/google_trend/google_trend_view_model.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'package:html_unescape/html_unescape.dart';
+import 'package:intl/intl.dart';
 
+import 'package:trender/assets/app_constants.dart';
 import 'package:trender/data/model/daily_trends_response.dart';
 import 'package:trender/domain/google_trend/get_daily_trends_use_case.dart';
 import 'package:trender/navigation_manager.dart';
@@ -14,29 +16,19 @@ class GoogleTrendViewModel with ChangeNotifier {
 
   GoogleTrendState state = GoogleTrendState.loading;
   List<DailyTrend> dailyTrends = <DailyTrend>[];
+  String endDateForNextRequest;
 
   GoogleTrendViewModel(
     this._navigationManager,
     this._getDailyTrendsUseCase,
   ) {
+    final DateTime now = DateTime.now();
+    final DateFormat formatter = DateFormat(yearMonthDay);
+    endDateForNextRequest = formatter.format(now);
     fetchDailyTrends();
   }
 
-  void fetchDailyTrends() {
-    state = GoogleTrendState.loading;
-    notifyListeners();
-
-    _getDailyTrendsUseCase('en', -480, 'US', '20210406', 15).then((response) {
-      final processedResponse = formatContent(response.dailyTrends);
-      dailyTrends.addAll(processedResponse);
-      state = GoogleTrendState.loaded;
-      notifyListeners();
-    }).catchError((error) {
-      state = GoogleTrendState.error;
-    });
-  }
-
-  List<DailyTrend> formatContent(List<DailyTrend> dailyTrends) {
+  List<DailyTrend> _formatContent(List<DailyTrend> dailyTrends) {
     final unescape = HtmlUnescape();
     return dailyTrends.map((dailyTrend) {
       dailyTrend.trends = dailyTrend.trends.map((trend) {
@@ -48,6 +40,29 @@ class GoogleTrendViewModel with ChangeNotifier {
       }).toList();
       return dailyTrend;
     }).toList();
+  }
+
+  void fetchDailyTrends() {
+    state = GoogleTrendState.loading;
+    notifyListeners();
+
+    if (endDateForNextRequest == null) {
+      final DateTime now = DateTime.now();
+      final DateFormat formatter = DateFormat(yearMonthDay);
+      endDateForNextRequest = formatter.format(now);
+    }
+
+    _getDailyTrendsUseCase('en', -480, 'US', endDateForNextRequest, 15)
+        .then((response) {
+      final processedResponse = _formatContent(response.dailyTrends);
+      dailyTrends.addAll(processedResponse);
+      endDateForNextRequest = response.endDateForNextRequest;
+      state = GoogleTrendState.loaded;
+      notifyListeners();
+    }).catchError((error) {
+      state = GoogleTrendState.error;
+      notifyListeners();
+    });
   }
 
   bool isLoading() {
